6d64a3af3263078179717167f3431de788b8b27a
robertbeaudenon <robertbeaudenon@gmail.com>
2019-12-17 15:04:58 -0500

    Added Jason Web Token Authentication when a user is created

--- backend/config/secret.js
+++ backend/config/secret.js
@@ -1,5 +1,6 @@
 //All our config will be centralized in that file so that we can modify one place and we are exporting it to make it available where we need it
 //chat app is the db name
 module.exports = {
-  url: 'mongodb://localhost:27017/chatapp'
+  url: 'mongodb://localhost:27017/chatapp',
+  secret: 'myjsonsecret'
 };
--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,8 +1,10 @@
 const Joi = require('@hapi/joi'); //Will help us validate the data that we are getting from the frontend before sending it to the db
-const HttpStatus = require('http-status-codes'); //instead of writing 200 we wirte H ttpStatus.GOOD_REQUEST
+const HttpStatus = require('http-status-codes'); //instead of writing 200 we wirte HttpStatus.GOOD_REQUEST
 const User = require('../models/userModels');
 const Helpers = require('../helpers/helper');
 const bcrypt = require('bcryptjs'); //to encrypt passwords
+const jwt = require('jsonwebtoken'); //tokens for authentication
+const dbConfig = require('../config/secret');
 
 module.exports = {
   //the call to the methods comes from Routes/authRoutes.js
@@ -25,12 +27,10 @@ module.exports = {
 
     //testing body against predefined schema and logging the error
     const { error, value } = schema.validate(req.body);
-    console.log(value);
     if (error && error.details) {
-      console.log(error.details);
       return res.status(HttpStatus.BAD_REQUEST).json({ message: error.details });
     }
-
+    // console.log('after schema validation');
     //verifies if the email already exist
     const userEmail = await User.findOne({ email: Helpers.lowerCase(req.body.email) }); //The functions need not to be chained one after another, simply await the function that returns the Promise.
     if (userEmail) {
@@ -43,7 +43,7 @@ module.exports = {
       return res.status(HttpStatus.CONFLICT).json({ meassage: 'Username already exist' });
     }
 
-    //Encrypting the password and returns it into 'hash'
+    //Encrypting the password and returns it into 'hash' var with a length of 10
     return bcrypt.hash(value.password, 10, (err, hash) => {
       if (err) {
         return res.status(HttpStatus.BAD_REQUEST).json({ meassage: 'Error hashing password' });
@@ -59,7 +59,12 @@ module.exports = {
       //We use the mongoose build in method to insert the object in the DB
       User.create(newBody)
         .then(user => {
-          res.status(HttpStatus.CREATED).json({ message: 'User created successfully', user });
+          //creating jwt token once the user is successfully created, (object, signature, options)
+          const token = jwt.sign({ data: user }, dbConfig.secret, {
+            expiresIn: 120
+          });
+
+          res.status(HttpStatus.CREATED).json({ message: 'User created successfully', user, token });
         })
         .catch(err => {
           res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Error occured' });
