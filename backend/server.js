5b1d5ea155e268286a0c6f6882bb8f7b5b413973
robertbeaudenon <robertbeaudenon@gmail.com>
2019-12-17 17:48:59 -0500

    Solved internal server error by adding CORS module to allow backend and frontend to communicate with each other

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,24 @@
 const express = require('express'); //express framework 'middleware'
 const mongoose = require('mongoose'); //Will allow to handle connection to MongoDB
 const cookieParser = require('cookie-parser'); //to save our token in the cookie
+const cors = require('cors'); //cross origin resource sharing (since backend and frontend are from different origin from each other, so this will allow front end to consume backend API)
 //const logger = require('morgan'); //will allow us to log REST operation with status
 
 //instance of express
 const app = express();
 
+app.use(cors());
+
 const dbConfig = require('./config/secret');
+
+//setting headers for application with allowed operations
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'DELETE', 'PUT', 'OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 app.use(express.json({ limit: '50mb' })); //we specify that we are returning or sending our data in the JSON format with a limit size of data
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
