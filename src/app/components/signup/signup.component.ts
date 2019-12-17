a3fe444922209385a3bd2606114608e68b617d3b
robertbeaudenon <robertbeaudenon@gmail.com>
2019-12-17 17:09:07 -0500

    Added the Reactive form module to handle registration and login

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,14 +8,29 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  //injecting the authService to be able to send data to the backend through it
-  constructor(private authService: AuthService) {}
+  //defining type of our form
+  signupForm: FormGroup;
 
-  ngOnInit() {}
+  //injecting the authService to be able to send data to the backend through it
+  constructor(private authService: AuthService, private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.init();
+  }
+
+  //we initialize the form and set validators to each one in case user forget to specify a field
+  init() {
+    this.signupForm = this.fb.group({
+      username: ['', Validators.required], //each username,email,password is piped from the HTML using the "formControlName"
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', Validators.required]
+    });
+  }
 
   signupUser() {
+    console.log(this.signupForm.value);
     //registerUser is the method defined in authService
-    this.authService.registerUser('test').subscribe(
+    this.authService.registerUser(this.signupForm.value).subscribe(
       data => {
         console.log(data);
       },
