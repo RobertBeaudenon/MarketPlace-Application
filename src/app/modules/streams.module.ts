4ee849f577a1f2dc12cc571a788ab30221feb7df
robertbeaudenon <robertbeaudenon@gmail.com>
2019-12-18 17:42:53 -0500

    Added side component that will contain informations and navigability

--- src/app/modules/streams.module.ts
+++ src/app/modules/streams.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { StreamsComponent } from '../components/streams/streams.component';
 import { TokenService } from '../services/token.service';
 import { ToolbarComponent } from '../components/toolbar/toolbar.component';
+import { SideComponent } from '../components/side/side.component';
+import { PostFormComponent } from '../components/post-form/post-form.component';
 
 @NgModule({
-  declarations: [StreamsComponent, ToolbarComponent],
+  declarations: [StreamsComponent, ToolbarComponent, SideComponent, PostFormComponent],
   imports: [CommonModule],
   exports: [StreamsComponent, ToolbarComponent],
   providers: [TokenService]
