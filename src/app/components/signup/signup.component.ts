2ceb1cbe5a44b5a30352ca0cc2c5526410f41652
robertbeaudenon <robertbeaudenon@gmail.com>
2019-12-17 19:26:50 -0500

    Added Spinner before redirecting user

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -12,6 +12,7 @@ export class SignupComponent implements OnInit {
   //defining type of our form
   signupForm: FormGroup;
   errorMessage: string;
+  showSpinner = false;
 
   //injecting the authService to be able to send data to the backend through it , fb for the formbuilder validations and ROuter to redirect to the desired component when registerd successfully
   constructor(private authService: AuthService, private fb: FormBuilder, private router: Router) {}
@@ -30,15 +31,19 @@ export class SignupComponent implements OnInit {
   }
 
   signupUser() {
+    this.showSpinner = true;
     console.log(this.signupForm.value);
     //registerUser is the method defined in authService
     this.authService.registerUser(this.signupForm.value).subscribe(
       data => {
         console.log(data);
         this.signupForm.reset(); //Reset form once signup
-        this.router.navigate(['streams']); //If signup successfull redirect user to component in path:streams (defined in streams-routing.module.ts)
+        setTimeout(() => {
+          this.router.navigate(['streams']); //If signup successfull redirect user to component in path:streams (defined in streams-routing.module.ts)
+        }, 3000);
       },
       err => {
+        this.showSpinner = false;
         //2 different types of error messages
         //if doesn't match predefined schema
         if (err.error.msg) {
