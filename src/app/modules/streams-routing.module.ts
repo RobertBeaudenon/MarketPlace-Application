80122822f50d0405d60ba268172f33e2700b503e
robertbeaudenon <robertbeaudenon@gmail.com>
2019-12-18 15:29:46 -0500

    Secured streams API by adding a guard that will prevent users to access endpoint without a valid token

--- src/app/modules/streams-routing.module.ts
+++ src/app/modules/streams-routing.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthTabsComponent } from '../components/auth-tabs/auth-tabs.component';
 import { StreamsComponent } from '../components/streams/streams.component';
+import { AuthGuard } from '../guards/auth.guard';
 
 //obj of routes
 const routes: Routes = [
   //default route
   {
     path: 'streams',
-    component: StreamsComponent
+    component: StreamsComponent,
+    canActivate: [AuthGuard] //if user has a valid token he will be able to access streams page otherwise he will be redirected to login/register page
   }
 ];
 
