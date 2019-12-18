dbf729fb59c1dede9796024ff0ff6daae25cc6e7
robertbeaudenon <robertbeaudenon@gmail.com>
2019-12-17 22:08:26 -0500

    Created the Login API

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router(); //allows us to create the routes
 
 //Authentication route using CreateUser method defined in controllers/auth.js
 const AuthCtrl = require('../controllers/auth');
+
 router.post('/register', AuthCtrl.CreateUser);
+router.post('/login', AuthCtrl.LoginUser);
 
 module.exports = router;
