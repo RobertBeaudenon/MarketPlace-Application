0f1e9ef6307309f8627035bceca43709bf90cf85
robertbeaudenon <robertbeaudenon@gmail.com>
2019-12-18 19:14:13 -0500

    Created the Post Collection for DB

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -1,10 +1,11 @@
 //to add mongoose schema that we are going to use in our db
 const mongoose = require('mongoose');
 
+//structure of user informations in DB
 const userSchema = mongoose.Schema({
   username: { type: String },
   email: { type: String },
   password: { type: String }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema); //User is the referance to the collection related to the userSchema in DB
