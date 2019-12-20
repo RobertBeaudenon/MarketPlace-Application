45e1e4d345c515f363c1f96d9cda4a1f7b759412
robertbeaudenon <robertbeaudenon@gmail.com>
2019-12-20 11:47:45 -0500

    Revert "Fixed Date Bug"
    This reverts commit 547236eaf73ce77279c09a7b75864d52dff389e3.

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -15,8 +15,4 @@ export class PostService {
   addPost(body): Observable<any> {
     return this.http.post(`${BASEURL}/post/add-post`, body);
   }
-
-  getAllPosts(): Observable<any> {
-    return this.http.get(`${BASEURL}/posts`);
-  }
 }
