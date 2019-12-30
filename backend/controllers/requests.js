cbffc0518bfc6ee89c3e561863fc80ed9f551067
robertbeaudenon <robertbeaudenon@gmail.com>
2019-12-30 12:32:34 +0200

    Notified user whos request was rejected

--- backend/controllers/requests.js
+++ backend/controllers/requests.js
@@ -95,23 +95,6 @@ module.exports = {
         }
       );
 
-      /******* Send notification to user whos request was rejected *******/
-      await User.update(
-        {
-          _id: req.body.userRequested //we look for the user that was rejected
-        },
-        {
-          $push: {
-            notifications: {
-              senderId: req.user._id,
-              message: `${req.user.username} rejected your request.`,
-              created: new Date(),
-              viewProfile: false
-            }
-          }
-        }
-      );
-
       /******* Update Requesters Array in User *******/
 
       await User.update(
@@ -183,6 +166,23 @@ module.exports = {
           $pull: {
             requesting: {
               postId: req.body.postId
+            }
+          }
+        }
+      );
+
+      /******* Send notification to user whos request was rejected *******/
+      await User.update(
+        {
+          _id: req.body.userRequested //we look for the user that was rejected
+        },
+        {
+          $push: {
+            notifications: {
+              senderId: req.user._id,
+              message: `${req.user.username} rejected your request.`,
+              created: new Date(),
+              viewProfile: false
             }
           }
         }
