2ccb897de14364c6c96154ea5646da2daa1c9b45
robertbeaudenon <robertbeaudenon@gmail.com>
2019-12-31 16:20:52 +0200

    Created 2 tabs in streams page to dispplay favorite posts

--- src/app/components/streams/streams.component.ts
+++ src/app/components/streams/streams.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenService } from 'src/app/services/token.service';
 import { Router } from '@angular/router';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-streams',
@@ -10,12 +11,12 @@ import { Router } from '@angular/router';
 export class StreamsComponent implements OnInit {
   constructor(private tokenService: TokenService, private router: Router) {}
 
-  //var of type any
   token: any;
 
   ngOnInit() {
     //if sign up successfull or login we are going to retreive the token in the streams page
     this.token = this.tokenService.GetPayload();
-    console.log(this.token);
+    const tabs = document.querySelector('.tabs');
+    M.Tabs.init(tabs, {});
   }
 }
