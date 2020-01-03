bc9885044396d40b2353dcf4dd13cc9b7dee5fce
robertbeaudenon <robertbeaudenon@gmail.com>
2020-01-03 19:50:55 +0200

    Saving ratings to DB

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
 app.use(express.json({ limit: '50mb' })); //we specify that we are returning or sending our data in the JSON format with a limit size of data
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
+app.use(express.static('public'));
 //app.use(logger('dev'));
 
 mongoose.Promise = global.Promise;
