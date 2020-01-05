7c611b5dd91fefd9839956dcf2c91e352add92bf
robertbeaudenon <robertbeaudenon@gmail.com>
2020-01-05 23:12:51 +0200

    Created the frontend message services to send message data to backend

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from 'src/app/services/token.service';
+import { MessageService } from 'src/app/services/message.service';
+import { ActivatedRoute } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-message',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
+  receiver: String;
+  user: any;
+  message: String; //whatever is typed in textarea of chat with the use of ngModel ="message" it will be retreived form the html into this var
+  receiverData: any;
 
-  constructor() { }
+  constructor(
+    private tokenService: TokenService,
+    private msgService: MessageService,
+    private route: ActivatedRoute,
+    private usersService: UsersService
+  ) {}
 
   ngOnInit() {
+    this.user = this.tokenService.GetPayload();
+
+    this.route.params.subscribe(params => {
+      this.receiver = params.name;
+      this.GetUserByUsername(this.receiver);
+    });
   }
 
+  GetUserByUsername(name) {
+    this.usersService.GetUserByName(name).subscribe(data => {
+      this.receiverData = data.result;
+    });
+  }
+
+  sendMessage() {
+    this.msgService
+      .SendMessage(this.user._id, this.receiverData._id, this.receiverData.username, this.message)
+      .subscribe(data => {
+        console.log(data);
+      });
+  }
 }
