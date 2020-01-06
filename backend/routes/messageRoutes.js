8bd3f0ba2bd66160140295e9a96495525f501941
robertbeaudenon <robertbeaudenon@gmail.com>
2020-01-06 07:31:18 -0500

    Created send message API to send message to a new user

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const messageCtrl = require('../controllers/message');
 const AuthHelper = require('../Helpers/authHelper');
 
-router.post('/chat-messages/:senderId/:receiverId', AuthHelper.VerifyToken, messageCtrl.SendMessage);
+router.post('/chat-messages/:sender_Id/:receiver_Id', AuthHelper.VerifyToken, messageCtrl.SendMessage);
 
 module.exports = router;
