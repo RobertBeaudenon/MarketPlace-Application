add72ccebe5abb609c615630969118f97009ef3f
robertbeaudenon <robertbeaudenon@gmail.com>
2020-01-06 08:45:54 -0500

    Created the get all messages API

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -6,5 +6,6 @@ const messageCtrl = require('../controllers/message');
 const AuthHelper = require('../Helpers/authHelper');
 
 router.post('/chat-messages/:sender_Id/:receiver_Id', AuthHelper.VerifyToken, messageCtrl.SendMessage);
+router.get('/chat-messages/:sender_Id/:receiver_Id', AuthHelper.VerifyToken, messageCtrl.GetAllMessages);
 
 module.exports = router;
