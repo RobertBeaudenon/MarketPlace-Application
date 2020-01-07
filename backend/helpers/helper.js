e387fdd6d19d711340b8f235f209d523a8756dd9
robertbeaudenon <robertbeaudenon@gmail.com>
2020-01-07 09:40:52 -0500

    Updated ChatList to display first last message received in notifications

--- backend/helpers/helper.js
+++ backend/helpers/helper.js
@@ -1,3 +1,5 @@
+const User = require('../models/userModels');
+
 module.exports = {
   //function that convert username to lowercase then set first letter to uppercase
   firstUpper: username => {
@@ -7,5 +9,72 @@ module.exports = {
 
   lowerCase: str => {
     return str.toLowerCase();
+  },
+
+  //When user send another message, chatlist array of receiver must be updated by deleting the previous last message and displaying the new last message in position 0 which means the first one in line
+  updateChatList: async (req, message) => {
+    await User.update(
+      {
+        _id: req.user._id
+      },
+      {
+        $pull: {
+          chatList: {
+            receiverId: req.params.receiver_id
+          }
+        }
+      }
+    );
+
+    await User.update(
+      {
+        _id: req.params.receiver_Id
+      },
+      {
+        $pull: {
+          chatList: {
+            receiverId: req.user._id
+          }
+        }
+      }
+    );
+
+    await User.update(
+      {
+        _id: req.user._id
+      },
+      {
+        $push: {
+          chatList: {
+            $each: [
+              {
+                receiverId: req.params.receiver_Id,
+                msgId: message._id
+              }
+            ],
+            $postion: 0
+          }
+        }
+      }
+    );
+
+    await User.update(
+      {
+        _id: req.params.receiver_Id
+      },
+      {
+        $push: {
+          chatList: {
+            $each: [
+              {
+                receiverId: req.user._id,
+                msgId: message._id
+              }
+            ],
+            $postion: 0
+          }
+        }
+      }
+    );
   }
 };
