42ce12acc2a2072f1222d94e08e8f7847e5657c1
robertbeaudenon <robertbeaudenon@gmail.com>
2020-01-07 18:57:26 -0500

    Added mark all messages as read functionality

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -8,5 +8,6 @@ const AuthHelper = require('../Helpers/authHelper');
 router.post('/chat-messages/:sender_Id/:receiver_Id', AuthHelper.VerifyToken, messageCtrl.SendMessage);
 router.get('/chat-messages/:sender_Id/:receiver_Id', AuthHelper.VerifyToken, messageCtrl.GetAllMessages);
 router.get('/receiver-messages/:sender/:receiver', AuthHelper.VerifyToken, messageCtrl.MarkReceiverMessages);
+router.get('/mark-all-messages', AuthHelper.VerifyToken, messageCtrl.MarkAllMessages);
 
 module.exports = router;
