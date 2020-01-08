52be709331df292012e08084682a58549e7e56ed
robertbeaudenon <robertbeaudenon@gmail.com>
2020-01-07 20:16:28 -0500

    Emitted online event

--- backend/socket/streams.js
+++ backend/socket/streams.js
@@ -1,5 +1,7 @@
 //we get io from our server.js
-module.exports = function(io) {
+module.exports = function(io, User, _) {
+  const user = new User();
+
   //we use the 'on' method to listen for events
   io.on('connection', socket => {
     socket.on('refresh', data => {
@@ -7,6 +9,13 @@ module.exports = function(io) {
       //console.log(data);
       io.emit('refreshPage', {}); //event name refreshPage, passing an emtpy object, all clinets connected will listen to the event, you can listen to it on any page using the socket.io url
     });
-    //console.log('user connected');
+
+    socket.on('online', data => {
+      socket.join(data.room);
+      user.EnterRoom(socket.id, data.user, data.room);
+      const list = user.GetList(data.room);
+      //send the set of users in room
+      io.emit('usersOnline', _.uniq(list));
+    });
   });
 };
