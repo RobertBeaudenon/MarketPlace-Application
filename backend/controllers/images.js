7ce806e8fc08a1aa037fbe668b3232d32d054260
robertbeaudenon <robertbeaudenon@gmail.com>
2020-01-12 17:42:40 -0500

    Created the set profile picture API

--- backend/controllers/images.js
+++ backend/controllers/images.js
@@ -17,7 +17,7 @@ module.exports = {
     //Uploading base64 encoded image to AWS S#
     buf = new Buffer(req.body.image.replace(/^data:image\/\w+;base64,/, ''), 'base64');
     const type = req.body.image.split(';')[0].split('/')[1];
-    console.log(type);
+
     const data = {
       Key: Date.now().toString(),
       Body: buf,
@@ -51,6 +51,21 @@ module.exports = {
       .then(() => res.status(HttpStatus.OK).json({ message: 'Image uploaded to AWS S3', location, key }))
       .catch(err =>
         res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Error Occured when uploading image to AWS S3' })
+      );
+  },
+
+  async SetDefaultImage(req, res) {
+    await User.update(
+      {
+        _id: req.user._id
+      },
+      {
+        picS3Key: req.params.imgS3Key
+      }
+    )
+      .then(() => res.status(HttpStatus.OK).json({ message: 'Profile picture updated' }))
+      .catch(err =>
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Error Occured when updating profile picture' })
       );
   }
 };
--- backend/routes/imageRoutes.js
+++ backend/routes/imageRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router(); //allows us to create the routes
 const ImageCtrl = require('../controllers/images');
 const AuthHelper = require('../Helpers/authHelper');
 
+router.get('/set-default-image/:imgS3Key', AuthHelper.VerifyToken, ImageCtrl.SetDefaultImage);
 router.post('/upload-image', AuthHelper.VerifyToken, ImageCtrl.UploadImage);
 
 module.exports = router;
