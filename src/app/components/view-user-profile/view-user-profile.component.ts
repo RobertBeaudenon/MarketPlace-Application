76ab392596a2d6731a4e94d2e8f873d1c824bd74
robertbeaudenon <robertbeaudenon@gmail.com>
2020-01-13 10:59:24 -0500

    Created layout of users profile component

--- src/app/components/view-user-profile/view-user-profile.component.ts
+++ src/app/components/view-user-profile/view-user-profile.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-view-user-profile',
   templateUrl: './view-user-profile.component.html',
   styleUrls: ['./view-user-profile.component.css']
 })
-export class ViewUserProfileComponent implements OnInit {
+export class ViewUserProfileComponent implements OnInit, AfterViewInit {
+  tabElement: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.tabElement = document.querySelector('.nav-content');
   }
 
+  ngAfterViewInit() {
+    const tabs = document.querySelector('.tabs');
+    M.Tabs.init(tabs, {});
+    this.tabElement.style.display = 'none';
+  }
 }
