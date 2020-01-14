f9f920f04cbe08b1ed6de4998d0ea18e191dd982
robertbeaudenon <robertbeaudenon@gmail.com>
2020-01-14 17:54:40 -0500

    Getting user name to retreive his info from params in route

--- src/app/components/view-user-profile/view-user-profile.component.ts
+++ src/app/components/view-user-profile/view-user-profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as M from 'materialize-css';
+import { ActivatedRoute } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
+import { arrayAverage } from '../../../assets/js/helpers.js';
 
 @Component({
   selector: 'app-view-user-profile',
@@ -10,16 +13,28 @@ export class ViewUserProfileComponent implements OnInit, AfterViewInit {
   tabElement: any;
   profileTab = true;
   postsTab = false;
+  posts = [];
+  average: any;
 
-  constructor() {}
+  user: any;
+  name: any;
+
+  constructor(private route: ActivatedRoute, private usersService: UsersService) {}
 
   ngOnInit() {
+    this.profileTab = true;
     this.tabElement = document.querySelector('.nav-content');
+    const tabs = document.querySelector('.tabs');
+    M.Tabs.init(tabs, {});
+
+    //we get name of user that we are inspecting profile throught the URL because we added it in the route ex: http://localhost:4200/name
+    this.route.params.subscribe(params => {
+      this.name = params.name;
+      this.GetUserData(this.name);
+    });
   }
 
   ngAfterViewInit() {
-    const tabs = document.querySelector('.tabs');
-    M.Tabs.init(tabs, {});
     this.tabElement.style.display = 'none';
   }
 
@@ -34,5 +49,18 @@ export class ViewUserProfileComponent implements OnInit, AfterViewInit {
       this.postsTab = false;
       this.profileTab = true;
     }
+  }
+
+  GetUserData(name) {
+    this.usersService.GetUserByName(name).subscribe(
+      data => {
+        console.log(data.result);
+        this.posts = data.result.posts;
+        this.user = data.result;
+        this.average = arrayAverage(this.user.ratingNumber);
+        //this.average = Math.round(this.average);
+      },
+      err => console.log(err)
+    );
   }
 }
