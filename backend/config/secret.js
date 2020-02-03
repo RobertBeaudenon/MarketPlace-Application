1e7e4cb1524b4e3efd0896e58ed907e4d4557f54
robertbeaudenon <robertbeaudenon@gmail.com>
2020-02-02 19:55:26 -0500

    Clean up ignored files

---
+++ backend/config/secret.js
@@ -0,0 +1,10 @@
+//All our config will be centralized in that file so that we can modify one place and we are exporting it to make it available where we need it
+//chat app is the db name
+module.exports = {
+  url: 'mongodb://localhost:27017/chatapp',
+  secret: '***REMOVED***',
+  AWS_SECRET_ACCESS: '***REMOVED***',
+  AWS_ACCESS_KEY: '***REMOVED***',
+  S3_BUCKET: '***REMOVED***',
+  TEST: '***REMOVED***'
+};
