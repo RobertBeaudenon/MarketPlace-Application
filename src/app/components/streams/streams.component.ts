9ca183c8826995b6d9d29f193d5023f1148c35b2
Robert <robertbeaudenon@gmail.com>
2020-03-21 20:44:44 -0400

    Added feedback textarea

--- src/app/components/streams/streams.component.ts
+++ src/app/components/streams/streams.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TokenService } from 'src/app/services/token.service';
 import { Router } from '@angular/router';
 import * as M from 'materialize-css';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-streams',
@@ -9,18 +10,28 @@ import * as M from 'materialize-css';
   styleUrls: ['./streams.component.css']
 })
 export class StreamsComponent implements OnInit {
-  constructor(private tokenService: TokenService, private router: Router) {}
+  constructor(private tokenService: TokenService, private router: Router, private fb: FormBuilder) {}
 
   token: any;
   streamsTab = false;
   favoriteStreamsTab = false;
   addPostTab = false;
+  loginForm: FormGroup;
+  errorMessage: string;
 
   ngOnInit() {
     this.streamsTab = true;
     this.token = this.tokenService.GetPayload();
     const tabs = document.querySelector('.tabs');
     M.Tabs.init(tabs, {});
+    this.init();
+  }
+
+  //we initialize the form and set validators to each one in case user forget to specify a field
+  init() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required] //each username,email,password is piped from the HTML using the "formControlName"
+    });
   }
 
   ChangeTabs(value) {
@@ -41,5 +52,9 @@ export class StreamsComponent implements OnInit {
       this.favoriteStreamsTab = false;
       this.addPostTab = true;
     }
+  }
+
+  submit() {
+    this.loginForm.reset();
   }
 }
